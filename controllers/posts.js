c338f068e00fc99d70c009c85b802857cb449cd4
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-02-04 22:30:23 -0800

    update: users can submit and save posts.

---
+++ controllers/posts.js
@@ -0,0 +1,29 @@
+ // ./controllers/posts.js
+
+
+const Post = require('../models/post');
+
+
+module.exports = (app) => {
+
+    // index
+    app.get('/', (req, res) => {
+        res.render('home', { msg: 'Handlebars are Cool!'})
+    });
+
+    // new
+    app.get('/posts/new', (req, res) => {
+        res.render('posts-new');
+    });
+
+    //Create
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+
+        // SAVE INSTANCE OF POST MODEL TO DB
+        post.save((err, post) => {
+            return res.redirect(`/`);
+        })
+    });
+}
--- server.js
+++ server.js
@@ -2,23 +2,25 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express()
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+// Set db
+const db = require('./data/reddit-db');
+
+//use body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//add after body parser initialization!
+app.use(expressValidator());
 
 
 app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main'}));
 app.set('view engine', '.hbs');
 
 
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!'})
-});
-
-
-
-
-
-
-
-
+const controllers = require('./controllers/posts')(app);
 
 
 
