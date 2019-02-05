58a453c3cae846bb320668766cfcd154dea3b9b9
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-02-05 01:18:57 -0800

    update: chai test install successfully

---
+++ test/index.js
@@ -0,0 +1,19 @@
+const app = require("./../server");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('site', function() {
+    // Describe what you are testing
+    it('Should have home page', function(done) {
+        chai.request(app).get('/').end(function(err, res) {
+            if (err) {
+                return done(err);
+            }
+            res.status.should.be.equal(200);
+            return done();
+        });
+    })
+});
