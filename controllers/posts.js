90d0705ecf88703df96fdffec9badd9d6c8ed53c
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-02-16 19:09:55 -0800

    [add] user for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,6 @@
  // ./controllers/posts.js
 
-
+const User = require('../models/user');
 const Post = require('../models/post');
 
 
@@ -10,7 +10,7 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         var currentUser = req.user;
         console.log(req.cookies);
-        Post.find({}).populate('author').then(posts => {
+        Post.find().populate(`author`).then(posts => {
             res.render('posts-index.hbs', { posts, currentUser });
         }).catch( err => {
             console.log(err.message);
@@ -19,27 +19,31 @@ module.exports = (app) => {
 
     // new
     app.get('/posts/new', (req, res) => {
-        res.render('posts-new.hbs', { });
+        var currentUser = req.user;
+        res.render('posts-new.hbs', { currentUser });
     });
 
     // create
     app.post('/posts/new', (req, res) => {
         if (req.user) {
             var post = new Post(req.body);
+            post.author =req.user._id;
 
-            post.save(function(err, post) {
-                return res.redirect(`/`);
+            post.save().then(post => {
+                return User.findById(req.user._id);
+
+            }).then(user => {
+                user.posts.unshift(post);
+                user.save();
+                // REDIRECT TO THE NEW Post
+                res.redirect(`/posts/${post._id}`);
+
+            }).catch(err => {
+                console.log(err.message);
             });
         } else {
             return res.status(401); // unauthorized
         }
-        // // INSTANTIATE INSTANCE OF POST MODEL
-        // const post = new Post(req.body);
-        // console.log(post);
-        // // SAVE INSTANCE OF POST MODEL TO DB
-        // post.save((err, post) => {
-        //     return res.redirect(`/`);
-        // })
     });
 
     // show
@@ -55,9 +59,9 @@ module.exports = (app) => {
 
     // SUBREDDIT
     app.get("/n/:subreddit", function(req, res) {
-        // var currentUser = req.user;
-        Post.find({ subreddit: req.params.subreddit }).then(posts => {
-            res.render("posts-index.hbs", { posts });
+        var currentUser = req.user;
+        Post.find({ subreddit: req.params.subreddit }).populate('author').then(posts => {
+            res.render("posts-index.hbs", { posts, currentUser });
         }).catch(err => {
             console.log(err.message);
         });
